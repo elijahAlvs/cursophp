c7b52e6b25847930f7f3dd79e7a96490510d591a
eliasAlvs <137322132+elijahAlvs@users.noreply.github.com>
2023-09-19 20:05:59 -0300

    Operadores aritméticos

---
+++ operadores_aritmeticos/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=,, initial-scale=1.0">
+    <title>Operadores Aritméticos</title>
+</head>
+<body>
+    <?php 
+    /*
+    + serve para adição                         5 + 2 = 7
+    . serve para concatenação                   5 - 2 = 3
+    * multiplicação                             5 * 2 = 10
+    / divisão                                   5 / 2 = 2.5
+    % calcula o resto da divisão inteira        5 % 2 = 1       (5 / 2 com resto 1 para não dar decimal)
+    ** potência                                5 ** 2 = 25
+    */
+    $k = "2" + "2";
+    var_dump($k);
+    $kk = "2" . "2";
+    var_dump($kk)
+    ?>
+</body>
+</html>
