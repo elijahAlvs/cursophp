69135174460e1753a082f5c4e84920598541c7d1
Elias Alves Lima <elias.alvslima@gmail.com>
2023-09-22 09:40:49 -0300

    Desafio 01 totalmente concluído
    Concluí totalmente o desafio 01 e já corrigi alguns problemas.

--- desafios/desafio01/desafio1.php
+++ desafios/desafio01/desafio1.php
@@ -6,13 +6,19 @@
     <title>Desafio 01 - 2° página</title>
 </head>
 <body>
-    <?php 
-    $numDoInput = $_GET["numInput"];
-    $antecessor = $numDoInput - 1;
-    $sucessor = $numDoInput + 1;
-    echo "<p>O número escolhido foi: $numDoInput</p>";
-    echo "<p>O seu antecessor é: $antecessor</p>";
-    echo "<p><p>O seu sucessor é: $sucessor</p>";
-    ?>
+    <p>
+        <?php
+        $numDoInput = $_GET["numInput"];
+        $antecessor = $numDoInput - 1;
+        $sucessor = $numDoInput + 1;
+        echo "<p>O número escolhido foi: <strong>$numDoInput</strong></p> <br>";
+        echo "<p>O seu antecessor é: $antecessor</p> <br>";
+        echo "<p>O seu sucessor é: $sucessor</p>";
+        ?>
+    </p>
+    <!-- 
+        history.go(-1) volta uma página no seu histórico de páginas.
+    -->
+    <button onclick="history.go(-1)">Voltar</button>
 </body>
 </html>
